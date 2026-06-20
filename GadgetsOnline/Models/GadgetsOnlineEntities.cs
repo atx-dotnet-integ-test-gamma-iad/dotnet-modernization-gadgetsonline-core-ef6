00695e96b0a8a772f1381efb264d60fab5183c19
NoReply Amazon <no-reply@amazon.com>
2026-06-20 04:37:05 +0000

    DB transformation: Upload transformed code

--- GadgetsOnline/Models/GadgetsOnlineEntities.cs
+++ GadgetsOnline/Models/GadgetsOnlineEntities.cs
@@ -1,9 +1,21 @@
 using GadgetsOnline.Models;
+using Npgsql;
+using System;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace GadgetsOnline.Models
 {
+    public class GadgetsOnlineEntitiesPostgreSqlConfiguration : System.Data.Entity.DbConfiguration
+    {
+        public GadgetsOnlineEntitiesPostgreSqlConfiguration()
+        {
+            SetProviderServices("Npgsql", NpgsqlServices.Instance);
+            SetDefaultConnectionFactory(new NpgsqlConnectionFactory());
+        }
+    }
+
+    [System.Data.Entity.DbConfigurationType(typeof(GadgetsOnlineEntitiesPostgreSqlConfiguration))]
     public class GadgetsOnlineEntities : DbContext
     {
         // Default constructor using connection string name from config
@@ -19,6 +31,38 @@ namespace GadgetsOnline.Models
         {
             this.Configuration.LazyLoadingEnabled = true;
             this.Configuration.ProxyCreationEnabled = true;
+        }
+
+        public override int SaveChanges()
+        {
+            FixDateTimeKinds();
+            return base.SaveChanges();
+        }
+
+        public override System.Threading.Tasks.Task<int> SaveChangesAsync(System.Threading.CancellationToken cancellationToken = default)
+        {
+            FixDateTimeKinds();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void FixDateTimeKinds()
+        {
+            var entries = ChangeTracker.Entries();
+            foreach (var entry in entries)
+            {
+                if (entry.State == System.Data.Entity.EntityState.Added ||
+                    entry.State == System.Data.Entity.EntityState.Modified)
+                {
+                    foreach (var property in entry.CurrentValues.PropertyNames)
+                    {
+                        var value = entry.CurrentValues[property];
+                        if (value is DateTime dateTime && dateTime.Kind != DateTimeKind.Utc)
+                        {
+                            entry.CurrentValues[property] = DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+                        }
+                    }
+                }
+            }
         }
 
         public DbSet<Product> Products { get; set; }
@@ -49,10 +93,51 @@ namespace GadgetsOnline.Models
                 .HasRequired(od => od.Product)
                 .WithMany()
                 .HasForeignKey(od => od.ProductId);
+
+            // PostgreSQL table and column mappings
+            modelBuilder.Entity<Product>().ToTable("products", "gadgetsonline_dbo");
+            modelBuilder.Entity<Product>().Property(e => e.ProductId).HasColumnName("productid");
+            modelBuilder.Entity<Product>().Property(e => e.CategoryId).HasColumnName("categoryid");
+            modelBuilder.Entity<Product>().Property(e => e.Name).HasColumnName("name");
+            modelBuilder.Entity<Product>().Property(e => e.Price).HasColumnName("price");
+            modelBuilder.Entity<Product>().Property(e => e.ProductArtUrl).HasColumnName("productarturl");
+
+            modelBuilder.Entity<Category>().ToTable("categories", "gadgetsonline_dbo");
+            modelBuilder.Entity<Category>().Property(e => e.CategoryId).HasColumnName("categoryid");
+            modelBuilder.Entity<Category>().Property(e => e.Name).HasColumnName("name");
+            modelBuilder.Entity<Category>().Property(e => e.Description).HasColumnName("description");
+
+            modelBuilder.Entity<Cart>().ToTable("carts", "gadgetsonline_dbo");
+            modelBuilder.Entity<Cart>().Property(e => e.RecordId).HasColumnName("recordid");
+            modelBuilder.Entity<Cart>().Property(e => e.CartId).HasColumnName("cartid");
+            modelBuilder.Entity<Cart>().Property(e => e.ProductId).HasColumnName("productid");
+            modelBuilder.Entity<Cart>().Property(e => e.Count).HasColumnName("count");
+            modelBuilder.Entity<Cart>().Property(e => e.DateCreated).HasColumnName("datecreated");
+
+            modelBuilder.Entity<Order>().ToTable("orders", "gadgetsonline_dbo");
+            modelBuilder.Entity<Order>().Property(e => e.OrderId).HasColumnName("orderid");
+            modelBuilder.Entity<Order>().Property(e => e.OrderDate).HasColumnName("orderdate");
+            modelBuilder.Entity<Order>().Property(e => e.Username).HasColumnName("username");
+            modelBuilder.Entity<Order>().Property(e => e.FirstName).HasColumnName("firstname");
+            modelBuilder.Entity<Order>().Property(e => e.LastName).HasColumnName("lastname");
+            modelBuilder.Entity<Order>().Property(e => e.Address).HasColumnName("address");
+            modelBuilder.Entity<Order>().Property(e => e.City).HasColumnName("city");
+            modelBuilder.Entity<Order>().Property(e => e.State).HasColumnName("state");
+            modelBuilder.Entity<Order>().Property(e => e.PostalCode).HasColumnName("postalcode");
+            modelBuilder.Entity<Order>().Property(e => e.Country).HasColumnName("country");
+            modelBuilder.Entity<Order>().Property(e => e.Phone).HasColumnName("phone");
+            modelBuilder.Entity<Order>().Property(e => e.Email).HasColumnName("email");
+            modelBuilder.Entity<Order>().Property(e => e.Total).HasColumnName("total");
+
+            modelBuilder.Entity<OrderDetail>().ToTable("orderdetails", "gadgetsonline_dbo");
+            modelBuilder.Entity<OrderDetail>().Property(e => e.OrderDetailId).HasColumnName("orderdetailid");
+            modelBuilder.Entity<OrderDetail>().Property(e => e.OrderId).HasColumnName("orderid");
+            modelBuilder.Entity<OrderDetail>().Property(e => e.ProductId).HasColumnName("productid");
+            modelBuilder.Entity<OrderDetail>().Property(e => e.Quantity).HasColumnName("quantity");
+            modelBuilder.Entity<OrderDetail>().Property(e => e.UnitPrice).HasColumnName("unitprice");
         }
 
     }
 
 
 }
-
